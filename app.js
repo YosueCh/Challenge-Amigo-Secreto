052d9100e779ea959a49433703c4c4dfd0c46b06
Yosue Chavez <s.yosue.chavez@gmail.com>
2025-02-25 16:36:45 -0600

    FUNCTION and ARRAY to add friend CREATED

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigosList = [];
+
+
+function agregarAmigo(){
+    let amigo = document.getElementById ('amigo').value;
+
+    if(amigo === ""){
+        alert('Por favor, inserte un nombre')
+    }else{
+        amigosList.push(amigo);
+        console.log(amigosList);
+        document.getElementById('amigo').value = '';
+    }
+    return amigosList;
+    
+}
